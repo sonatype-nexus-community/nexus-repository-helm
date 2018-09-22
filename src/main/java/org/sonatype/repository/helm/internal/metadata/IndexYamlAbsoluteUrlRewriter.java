6ba799d46ec0e19dbe8ba156ff2c85e05fad1ae8
ylf <1045438139@qq.com>
2018-09-22 10:45:29 +0800

    feat(secondary-directory): Support for secondary directory to get helm repo
    1. Support for such a repo and is compatible with the previous
    https://burdenbear.github.io/kube-charts-mirror/

--- src/main/java/org/sonatype/repository/helm/internal/metadata/IndexYamlAbsoluteUrlRewriter.java
+++ src/main/java/org/sonatype/repository/helm/internal/metadata/IndexYamlAbsoluteUrlRewriter.java
@@ -108,10 +108,9 @@ public class IndexYamlAbsoluteUrlRewriter
       URI uri = new URIBuilder(oldUrl).build();
       if (uri.isAbsolute()) {
         String fileName = uri.getPath();
-        // Strip leading slash
-        if (!fileName.isEmpty()) {
-            fileName = fileName.substring(1);
-        }
+        //remove leading "/" to avoid URI missing, by ref: https://github.com/helm/helm/issues/3878
+        //fix https://github.com/sonatype-nexus-community/nexus-repository-helm/issues/14
+        fileName = fileName.startsWith("/") ? fileName.substring(fileName.lastIndexOf("/")+1) : fileName;
         scalarEvent = new ScalarEvent(scalarEvent.getAnchor(), scalarEvent.getTag(),
             scalarEvent.getImplicit(), fileName, scalarEvent.getStartMark(),
             scalarEvent.getEndMark(), scalarEvent.getStyle());
