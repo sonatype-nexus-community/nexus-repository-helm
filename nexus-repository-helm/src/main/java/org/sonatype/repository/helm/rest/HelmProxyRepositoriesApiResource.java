9c4c8185af264df209faf70ae82c006ef6360cfb
Maksym Lukaretkyi <56254062+mlukaretkyi@users.noreply.github.com>
2020-01-15 13:18:17 +0200

    NEXUS-22443: Helm REST API does not work (#111)

--- nexus-repository-helm/src/main/java/org/sonatype/repository/helm/rest/HelmProxyRepositoriesApiResource.java
+++ nexus-repository-helm/src/main/java/org/sonatype/repository/helm/rest/HelmProxyRepositoriesApiResource.java
@@ -15,21 +15,38 @@ package org.sonatype.repository.helm.rest;
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.inject.Singleton;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
 
 import org.sonatype.nexus.repository.rest.api.AbstractRepositoriesApiResource;
 import org.sonatype.nexus.repository.rest.api.AbstractRepositoryApiRequestToConfigurationConverter;
 import org.sonatype.nexus.repository.rest.api.AuthorizingRepositoryManager;
 import org.sonatype.nexus.repository.rest.api.RepositoriesApiResource;
-import org.sonatype.nexus.rest.Resource;
+import org.sonatype.nexus.validation.Validate;
 import org.sonatype.repository.helm.api.HelmProxyRepositoryApiRequest;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.apache.shiro.authz.annotation.RequiresAuthentication;
+
+import static org.sonatype.nexus.rest.ApiDocConstants.API_REPOSITORY_MANAGEMENT;
+import static org.sonatype.nexus.rest.ApiDocConstants.AUTHENTICATION_REQUIRED;
+import static org.sonatype.nexus.rest.ApiDocConstants.INSUFFICIENT_PERMISSIONS;
+import static org.sonatype.nexus.rest.ApiDocConstants.REPOSITORY_CREATED;
+import static org.sonatype.nexus.rest.ApiDocConstants.REPOSITORY_UPDATED;
+
+@Api(value = API_REPOSITORY_MANAGEMENT)
 @Named
 @Singleton
 @Path(RepositoriesApiResource.RESOURCE_URI + "/helm/proxy")
 public class HelmProxyRepositoriesApiResource
     extends AbstractRepositoriesApiResource<HelmProxyRepositoryApiRequest>
-    implements Resource, HelmProxyRepositoriesApiResourceDoc
 {
   @Inject
   public HelmProxyRepositoriesApiResource(
@@ -37,5 +54,37 @@ public class HelmProxyRepositoriesApiResource
       final AbstractRepositoryApiRequestToConfigurationConverter<HelmProxyRepositoryApiRequest> configurationAdapter)
   {
     super(authorizingRepositoryManager, configurationAdapter);
+  }
+
+  @ApiOperation("Create Helm proxy repository")
+  @ApiResponses(value = {
+      @ApiResponse(code = 201, message = REPOSITORY_CREATED),
+      @ApiResponse(code = 401, message = AUTHENTICATION_REQUIRED),
+      @ApiResponse(code = 403, message = INSUFFICIENT_PERMISSIONS)
+  })
+  @POST
+  @RequiresAuthentication
+  @Validate
+  @Override
+  public Response createRepository(final HelmProxyRepositoryApiRequest request) {
+    return super.createRepository(request);
+  }
+
+  @ApiOperation("Update Helm proxy repository")
+  @ApiResponses(value = {
+      @ApiResponse(code = 204, message = REPOSITORY_UPDATED),
+      @ApiResponse(code = 401, message = AUTHENTICATION_REQUIRED),
+      @ApiResponse(code = 403, message = INSUFFICIENT_PERMISSIONS)
+  })
+  @PUT
+  @Path("/{repositoryName}")
+  @RequiresAuthentication
+  @Validate
+  @Override
+  public Response updateRepository(
+      final HelmProxyRepositoryApiRequest request,
+      @ApiParam(value = "Name of the repository to update") @PathParam("repositoryName") final String repositoryName)
+  {
+    return super.updateRepository(request, repositoryName);
   }
 }
